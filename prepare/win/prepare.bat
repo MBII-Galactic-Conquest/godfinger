a738be01302c92cb415c80977d4dc4dee160c506
2cwldys <sdpayne113@gmail.com>
2025-03-01 03:41:02 -0500

    [Removal] unnecessary python checks in prepare.bat

--- prepare/win/prepare.bat
+++ prepare/win/prepare.bat
@@ -4,26 +4,9 @@
 cd ../
 cd bin 2>nul || goto bin_missing
 
-:: Check if Python 3 is installed, fallback to Python
-where python3 >nul 2>nul
-if %errorlevel% == 0 (
-    set PYTHON_CMD=python
-    set PIP_CMD=pip
-) else (
-    where python >nul 2>nul
-    if %errorlevel% == 0 (
-        set PYTHON_CMD=python
-        set PIP_CMD=pip
-    ) else (
-        echo Error: Python is not installed.
-        pause
-        exit /b
-    )
-)
-
 :: Install dependencies
 echo Installing dependencies...
-%PIP_CMD% install -U -r requirements.txt
+python -m pip install -U -r requirements.txt
 if %errorlevel% neq 0 (
     echo Error installing dependencies. Press Enter to exit.
     pause
@@ -32,7 +15,7 @@ if %errorlevel% neq 0 (
 
 :: Run the prepare.py script
 echo Running prepare.py...
-START /WAIT %PYTHON_CMD% ./prepare.py
+START /WAIT python ./prepare.py
 if %errorlevel% neq 0 (
     echo Error running prepare.py. Press Enter to exit.
     pause
