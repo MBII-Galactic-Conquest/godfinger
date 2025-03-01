b805ff5c07bae0ee4668e008bdf319f35342dcb7
ViceDice <horrible023@gmail.com>
2025-03-02 01:04:17 +0500

    [Fix] win startup echo stuff

--- start/win/startDebug.bat
+++ start/win/startDebug.bat
@@ -1,12 +1,12 @@
-
+@echo off
 REM Check Python version
 python -c "import sys; sys.exit(0 if sys.version_info >= (3, 12) else 1)"
 IF %ERRORLEVEL% NEQ 0 (
+    @echo on
     echo Error: Python 3.12+ is required but not found.
     echo Please install Python 3.12 or higher and try again.
     exit /b 1
 )
-echo off
 CD ../../
 set "venvp=./venv/Scripts/activate.bat"
 if exist "%venvp%" (
@@ -18,7 +18,7 @@ if exist "%venvp%" (
     python ./godfinger.py --debug
     PAUSE
 ) else (
-    echo on
+    @echo on
     echo Virtual environment does not exist or was created improperly, please run prepare.bat in root dir, aborting.
     PAUSE
 )
