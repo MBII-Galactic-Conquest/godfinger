2c9ebc0ecfbc4c489bd3676851c6ad20c75afd04
2cwldys <sdpayne113@gmail.com>
2025-03-01 04:31:14 -0500

    [Change] Windows start checks if python is compatible

--- start/win/startDebugSilent.bat
+++ start/win/startDebugSilent.bat
@@ -1,7 +1,18 @@
+@echo off
+
+REM Check Python version
+python -c "import sys; sys.exit(0 if sys.version_info >= (3, 12) else 1)"
+IF %ERRORLEVEL% NEQ 0 (
+    echo Error: Python 3.12+ is required but not found.
+    echo Please install Python 3.12 or higher and try again.
+    exit /b 1
+)
+
+REM If Python 3.12+ is found, continue with the rest of the commands
 CD ../../
 CD ./update
-python ./update.py
+PYTHON ./update.py
 CD ../
 CALL ./cleanup.bat
-python ./godfinger.py --debug -lf "./bigdata.log"
+PYTHON ./godfinger.py --debug -lf "./bigdata.log"
 PAUSE
