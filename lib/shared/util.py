9818b140c7ed23f18b37ef265133a906e4e52e3e
ViceDice <horrible023@gmail.com>
2025-03-04 00:26:45 +0500

    [OOF] wrong file

--- lib/shared/util.py
+++ lib/shared/util.py
@@ -1,29 +1,3 @@
-import traceback;
-import inspect;
 
 def Clamp(minimum, x, maximum):
     return max(minimum, min(x, maximum))
-
-
-def FullTracebackString(tb_frame) -> str:
-    result = "";
-    if tb_frame != None:
-        return result;
-
-
-
-def TestNest(argumentvar1, argumentvar2):
-    localVar = 1234;
-    localString = "bunghole";
-    raise Exception("FUCK");
-
-def Test():
-    print("\nAYYY CARUMBA");
-    try:
-        TestNest(1337, "Fucking Idiot");
-    except Exception as sex:
-        print(str(inspect.trace()[-1][0].f_locals));
-
-
-if __name__ == "__main__":
-    Test();
