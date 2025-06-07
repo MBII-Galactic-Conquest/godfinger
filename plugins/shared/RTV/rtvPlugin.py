fb432b5560350e5ed38f70904de5f8e3839ecaca
mantlar <irolph@hotmail.com>
2025-06-06 19:27:41 -0700

    [Feature] RTVRTM credits

--- plugins/shared/RTV/rtvPlugin.py
+++ plugins/shared/RTV/rtvPlugin.py
@@ -1,3 +1,50 @@
+# ORIGINAL RTVRTM SCRIPT CREDITS:
+# =================================================================================
+# Copyright (c) 2012-2013, klax / Cthulhu@GBITnet.com.br
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#################################################
+#          Movie Battles II RTV/RTM             #
+#                                               #
+#      Rock the Vote and Rock the Mode tool     #
+#      for Jedi Knight: Jedi Academy            #
+#      Movie Battles II MOD.                    #
+#      Original plugin and idea by:             #
+#      AlliedModders LLC. All rights reserved.  #
+#################################################
+# =================================================================================
+# GODFINGER RTV CREDITS:
+# =================================================================================
+# Godfinger created by:
+# 2cwldys (https://github.com/2cwldys),
+# ACHUTA/Mantlar, (https://github.com/mantlar)
+# ViceDice, (https://github.com/2cwldys)
+# Wookiee- (https://github.com/Wookiee-)
+#
+# RTV plugin created by ACHUTA
+
+
+
 from enum import Enum, auto
 import json
 import logging
